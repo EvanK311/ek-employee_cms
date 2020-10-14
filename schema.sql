160af7ab1a610965050cf236e473b746c73b6d95
Evan K <EvanK311@gmail.com>
2020-10-13 23:12:46 -0700

    add employees function added

--- schema.sql
+++ schema.sql
@@ -4,25 +4,25 @@ CREATE database employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE Department (
-  id INT NOT NULL AUTO INCREMENT,
-  names VARCHAR(30) NULL,
-   PRIMARY KEY(id) 
+ id INT AUTO_INCREMENT NOT NULL,
+ names VARCHAR(30) NULL,
+ PRIMARY KEY (id) 
 );
 
 CREATE TABLE Role (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     Title VARCHAR(30),
     Salary DECIMAL(6,2),
-    Department_id INT 
+    Department_id INT, 
     FOREIGN KEY(department_id) REFERENCES department(id),
     PRIMARY KEY (id)
-)
+);
 
-CREATE TABLE Employee (
+CREATE TABLE employee (
     id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT
+    role_id INT,
     manager_id INT 
     )
 
